2ff7b79723dc4fb0191197c731d6c2837a33d345
DANTE GARCIA RAMIREZ <dante@oomba.com>
2015-05-28 15:06:26 -0700

    starting to implement script

--- Models/preLoadedMessages.py
+++ Models/preLoadedMessages.py
@@ -1,8 +1,10 @@
 import sys
 
-messages = ["Welcome!", #Code: 0
- "What is your name? "] #Code: 1
 
+messages = ["Welcome to Socks and Shirts!", #Code: 0
+ "What is your name? ", #Code: 1
+ "Thank you, ", #Code: 2
+ "How can we assist you today?\n\t1) Order shirts\n\t2) Questions\n\t3) Complaints\n"] #Code: 3
 def getPreloadedMessages(messageCode):
  global messages
  return messages[messageCode]
--- Views/clientView.py
+++ Views/clientView.py
@@ -10,6 +10,9 @@ def startClient():
   print messageFetcher.fetchMessage(0)
   global name
   name = raw_input(messageFetcher.fetchMessage(1)).rstrip()
+  print (messageFetcher.fetchMessage(2) + name)
+  raw_input(messageFetcher.fetchMessage(3)).rstrip()
+  
   return name
 def getUserInput():
   global name
