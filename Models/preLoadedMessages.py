93e4cf72186320b4efa163fc413388129d28654e
DANTE GARCIA RAMIREZ <dante@oomba.com>
2015-05-28 15:16:42 -0700

    Script implemented in client view

--- Models/preLoadedMessages.py
+++ Models/preLoadedMessages.py
@@ -4,7 +4,9 @@ import sys
 messages = ["Welcome to Socks and Shirts!", #Code: 0
  "What is your name? ", #Code: 1
  "Thank you, ", #Code: 2
- "How can we assist you today?\n\t1) Order shirts\n\t2) Questions\n\t3) Complaints\n"] #Code: 3
+ "How can we assist you today?\n\t1) Order shirts\n\t2) Questions\n\t3) Complaints\n", #code 3
+ "Your selection was: ", #Code: 4
+ "Connecting you with an agent, please wait"] #code 5
 def getPreloadedMessages(messageCode):
  global messages
  return messages[messageCode]
--- Views/clientView.py
+++ Views/clientView.py
@@ -11,8 +11,18 @@ def startClient():
   global name
   name = raw_input(messageFetcher.fetchMessage(1)).rstrip()
   print (messageFetcher.fetchMessage(2) + name)
-  raw_input(messageFetcher.fetchMessage(3)).rstrip()
-  
+  menuSelection = "0"
+  while menuSelection == "0":
+    menuSelection = raw_input(messageFetcher.fetchMessage(3)).rstrip()
+    if menuSelection == "1":
+      print(messageFetcher.fetchMessage(4) + "Order shirts")
+    elif menuSelection == "2":
+      print(messageFetcher.fetchMessage(4) + "Questions")
+    elif menuSelection == "3":
+      print(messageFetcher.fetchMessage(4) + "Complaints")
+    else:
+      menuSelection = "0"
+  print(messageFetcher.fetchMessage(5))
   return name
 def getUserInput():
   global name
